edc616307d6e70f00a011e8b9cfa1fafe04b8764
Vijay Karthik P <vjykrthk@gmail.com>
2012-08-30 19:14:11 +0530

    Change README.rdoc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterClone::Application.config.secret_token = '0d75cbd219c66a11f627d3656135b7ef09f1203ce1c64d77243a2c8649eed1948764bad978b3798989b1763e3af3816bc5bec41fb6ede284132138128979b0f9'
