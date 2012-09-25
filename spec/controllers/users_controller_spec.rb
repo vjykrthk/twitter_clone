771a4b77c990651623d55925588602e955a6883c
Vijay Karthik <vjykrthk@gmail.com>
2012-09-25 13:53:22 +0530

    Add rss feed

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe UsersController do 
+	let(:user) { FactoryGirl.create(:user) }
+	let(:m1) { FactoryGirl.create(:micropost, user:user)}
+	let(:m2) { FactoryGirl.create(:micropost, user:user)}
+
+	describe "RSS FEED" do
+		it "Should return feed in atom format" do
+			get :show, id: user, format:"atom"
+			response.should be_success
+			response.should render_template("users/show")
+			response.content_type.should eq("application/atom+xml")
+		end
+	end
+end
